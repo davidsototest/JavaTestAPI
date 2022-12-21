ae70c135807d9e3ab1e1036b0351baa83c5afb47
David Git Local <david.soto.test@gmail.com>
2022-12-21 19:04:14 -0300

    actualizado

--- src/main/java/com/decirHola/DecirHola/DecirHolaController.java
+++ src/main/java/com/decirHola/DecirHola/DecirHolaController.java
@@ -1,17 +1,30 @@
 
 package com.decirHola.DecirHola;
 
+import java.time.Instant;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping("/api/v1.0")
+
 public class DecirHolaController {
-    
+   
     @GetMapping ("/hola") // consulta a get con URL
     public String decirHola(){
         return "hola Mundo, David Soto";
+    }
+    
+    @GetMapping("/time")
+    @ResponseStatus(HttpStatus.OK)
+    public String getCurrentTime() {
+
+        return Instant.now().toString();
     }
     
     @GetMapping ("/hola/{nombre}/{apellido}/{edad}") // consulta a get con URL y parametro
